4857e20e2c9d69c74c37e955ac462e37d59dbe57
O'Brien <obriepau@oregonstate.edu>
2019-04-16 19:08:08 -0700

    32

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ var hideLabel = function(label){ label.labelObject.style.opacity = 0;};
 var showLabel = function(label){ label.labelObject.style.opacity = 1;};
 var labelEngine = new labelgun.default(hideLabel, showLabel);
 var labels = [];
-
 // 18. define the coordinate reference system (CRS)
 mycrs = new L.Proj.CRS('EPSG:2991',
     '+proj=lcc +lat_1=43 +lat_2=45.5 +lat_0=41.75 +lon_0=-120.5 +x_0=400000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
@@ -14,62 +13,46 @@ mycrs = new L.Proj.CRS('EPSG:2991',
         resolutions: [8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1] // example zoom level resolutions
     }
 );
-
 // 1. Create a map object.
 var mymap = L.map('map', {
+    crs: mycrs,  // 19. assign the custom crs to the crs option. change the zoom levels due to the change of projection.
     center: [44.13, -119.93],
-    zoom: 4,
+    zoom: 3, // we choose zoom level 3
     maxZoom: 10,
     minZoom: 3,
     detectRetina: true});
-
 // 2. Add a base map.
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(mymap);
-
+//  L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(mymap);
+// 20. comment off the base map, because the publicly shared base map usually cannot reprojected
+// L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(mymap);
 // 3. Add cell towers GeoJSON Data
 // Null variable that will hold cell tower data
 var airports = null;
-
-
 // 4. build up a set of colors from colorbrewer's dark2 category
 var colors = chroma.scale('Dark2').mode('lch').colors(9);
-
 // 5. dynamically append style classes to this page. This style classes will be used for colorize the markers.
 for (i = 0; i < 9; i++) {
     $('head').append($("<style> .marker-color-" + (i + 1).toString() + " { color: " + colors[i] + "; font-size: 15px; text-shadow: 0 0 3px #ffffff;} </style>"));
 }
-
 // Get GeoJSON and put on it on the map when it loads
-airports= L.geoJson.ajax("assets/airports.geojson", {
+airports= L.geoJson.ajax("Assets/airports.geojson", {
     // assign a function to the onEachFeature parameter of the cellTowers object.
     // Then each (point) feature will bind a popup window.
     // The content of the popup window is the value of `feature.properties.company`
     onEachFeature: function (feature, layer) {
-        layer.bindPopup(feature.properties.company);
+        layer.bindPopup(feature.properties.AIRPT_NAME);
     },
     pointToLayer: function (feature, latlng) {
         var id = 0;
-        if (feature.properties.CNTL_TWR == "Y") { id = 0; }
-        else if (feature.properties.CNTL_TWR == "N")  { id = 4; }
-        else { id = 2;} //
+        if (feature.properties.CNTL_TWR == "N") { id = 0; }
+        else if (feature.properties.CNTL_TWR == "Y") { id = 4; }
+        else { id = 2;} // "Salem Cellular"
         return L.marker(latlng, {icon: L.divIcon({className: 'fa fa-plane marker-color-' + (id + 1).toString() })});
     },
     attribution: 'Airport Data &copy; DATA.gov | States &copy; Mike Bostock | Base Map &copy; CartoDB | Made By Paul OBrien'
 }).addTo(mymap);
-
-// create the states layer
-var states = null;
-states= L.geoJson.ajax("assets/us-states.geojson", {
-    style: style
-}).addTo(mymap);
-onEachFeature: function (feature, label) {
-        label.bindTooltip(feature.properties.NAME, {className: 'feature-label', permanent:true, direction: 'center'});
-        labels.push(label);
-    }
-
 // 6. Set function for color ramp
-colors = chroma.scale('OrRd').colors(5); //colors = chroma.scale('RdPu').colors(5);
-
+colors = chroma.scale('OrRd').colors(5); //colors = chroma.scale('OrRd').colors(5);
 function setColor(density) {
     var id = 0;
     if (density > 18) { id = 4; }
@@ -79,8 +62,6 @@ function setColor(density) {
     else  { id = 0; }
     return colors[id];
 }
-
-
 // 7. Set style function that sets fill color.md property equal to cell tower density
 function style(feature) {
     return {
@@ -92,34 +73,40 @@ function style(feature) {
         dashArray: '4'
     };
 }
-
+// 8. Add county polygons
+// create counties variable, and assign null to it.
+// 15. Create a label for each county.
+var states = null;
+counties = L.geoJson.ajax("Assets/us-states.geojson", {
+    style: style,
+    onEachFeature: function (feature, label) {
+        label.bindTooltip(feature.properties.NAME, {className: 'feature-label', permanent:true, direction: 'center'});
+        labels.push(label);
+    }
+}).addTo(mymap);
 // 9. Create Leaflet Control Object for Legend
 var legend = L.control({position: 'topright'});
-
 // 10. Function that runs when legend is added to map
 legend.onAdd = function () {
-
     // Create Div Element and Populate it with HTML
     var div = L.DomUtil.create('div', 'legend');
-    div.innerHTML += '<b># of Airports</b><br />';
+    div.innerHTML += '<b># Of Airports</b><br />';
     div.innerHTML += '<i style="background: ' + colors[4] + '; opacity: 0.5"></i><p>19+</p>';
     div.innerHTML += '<i style="background: ' + colors[3] + '; opacity: 0.5"></i><p>14-18</p>';
     div.innerHTML += '<i style="background: ' + colors[2] + '; opacity: 0.5"></i><p>11-13</p>';
     div.innerHTML += '<i style="background: ' + colors[1] + '; opacity: 0.5"></i><p> 6-10</p>';
     div.innerHTML += '<i style="background: ' + colors[0] + '; opacity: 0.5"></i><p> 0- 5</p>';
-    div.innerHTML += '<hr><b>Airports<b><br />';
+    div.innerHTML += '<hr><b>Aiports<b><br />';
     div.innerHTML += '<i class="fa fa-plane marker-color-7"></i><p> With Control Tower</p>';
-    div.innerHTML += '<i class="fa fa-plane marker-color-9"></i><p> No Control Tower</p>';
+    div.innerHTML += '<i class="fa fa-plane marker-color-9"></i><p> With No Control Tower</p>';
     // Return the Legend div containing the HTML content
     return div;
 };
-
 // 11. Add a legend to map
 legend.addTo(mymap);
-
 // 12. Add a scale bar to map
-L.control.scale({position: 'bottomleft'}).addTo(mymap);
-
+// 21. comment off the scale bar, because in same project, scalebar does not make sense.
+// L.control.scale({position: 'bottomleft'}).addTo(mymap);
 // 13. Add a latlng graticules.
 L.latlngGraticule({
     showLabel: true,
@@ -130,18 +117,13 @@ L.latlngGraticule({
         {start: 8, end: 10, interval: 0.5}
     ]
 }).addTo(mymap);
-
-
 // 16. create an addLabel function to dynamically update the visible labels, aiming to avoid the lable overlap.
-
 function addLabel(layer, id) {
-
     // This is ugly but there is no getContainer method on the tooltip :(
     var label = layer.getTooltip()._source._tooltip._container;
     if (label) {
         // We need the bounding rectangle of the label itself
         var rect = label.getBoundingClientRect();
-
         // We convert the container coordinates (screen space) to Lat/lng
         var bottomLeft = mymap.containerPointToLatLng([rect.left, rect.bottom]);
         var topRight = mymap.containerPointToLatLng([rect.right, rect.top]);
@@ -149,7 +131,6 @@ function addLabel(layer, id) {
             bottomLeft : [bottomLeft.lng, bottomLeft.lat],
             topRight   : [topRight.lng, topRight.lat]
         };
-
         // Ingest the label into labelgun itself
         labelEngine.ingestLabel(
             boundingBox,
@@ -159,7 +140,6 @@ function addLabel(layer, id) {
             label.innerText,
             false
         );
-
         // If the label hasn't been added to the map already
         // add it and set the added flag to true
         if (!layer.added) {
@@ -168,7 +148,6 @@ function addLabel(layer, id) {
         }
     }
 }
-
 // 17. We will update the visualization of the labels whenever you zoom the map.
 mymap.on("zoomend", function(){
     var i = 0;
